efc225d42feff55008658630e1c8a34ee5e7b8e9
1804689260 <wxc2897764177@qq.com>
2022-08-07 22:09:46 +0800

    add hello

--- test/hello.js
+++ test/hello.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  hello
+// @author       Wxc
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @license           Apache-2.0
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('wxc');
+})();
